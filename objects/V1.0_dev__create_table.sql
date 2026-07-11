1d4f7cc8bf9f8adb871225c62f8983235fbabb3f
Sukh <sukhpreetsingh@gmail.com>
2026-07-11 22:06:21 +0530

    corrections3

--- objects/V1.0_dev__create_table.sql
+++ objects/V1.0_dev__create_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS IDENTIFIER('$DB_NAME' || '.FROM_DEV.CUSTOMERS') (
     customer_id     NUMBER(38,0)   NOT NULL,
     customer_name   VARCHAR(200)   NOT NULL,
-    email           VARCHAR(255),
+    email_id           VARCHAR(255),
     signup_date     DATE           NOT NULL,
     is_active       BOOLEAN        DEFAULT TRUE,
     created_at      TIMESTAMP_NTZ  DEFAULT CURRENT_TIMESTAMP()
