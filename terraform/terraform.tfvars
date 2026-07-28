faa8ae85f9778fc1f2aafefc80f3c4c78056d003
Sukh <sukhpreetsingh@gmail.com>
2026-07-28 13:00:48 +0530

    test

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -3,3 +3,11 @@ user         = "SUKHPREETSNOWFLAKE"
 password     = "Qwertyuiop@1234567890"
 role         = "ACCOUNTADMIN"
 warehouse    = "COMPUTE_WH"
+
+
+database_name       = "TERRAFORM_DB"
+warehouse_name      = "TF_APP_WH"
+warehouse_size      = "SMALL"
+app_role_name       = "TF_APP_ROLE"
+read_only_role_name = "TF_READ_ONLY_ROLE"
+schema_name         = "TF_APP_SCHEMA"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,13 +27,11 @@ variable "warehouse" {
 variable "database_name" {
   type        = string
   description = "Name of the application database"
-  default     = "TERRAFORM_DB"
 }
 
 variable "warehouse_name" {
   type        = string
   description = "Name of the application warehouse"
-  default     = "TF_APP_WH"
 }
 
 variable "warehouse_size" {
@@ -45,17 +43,14 @@ variable "warehouse_size" {
 variable "app_role_name" {
   type        = string
   description = "Name of the application role with full access"
-  default     = "TF_APP_ROLE"
 }
 
 variable "read_only_role_name" {
   type        = string
   description = "Name of the read-only role"
-  default     = "TF_READ_ONLY_ROLE"
 }
 
 variable "schema_name" {
   type        = string
   description = "Name of the application schema"
-  default     = "TF_APP_SCHEMA"
 }
