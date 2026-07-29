c30163a491b3107a0349ca920ee62dce83556d83
Sukh <sukhpreetsingh@gmail.com>
2026-07-29 17:13:19 +0530

    fergtyh

--- terraform/role.tf
+++ terraform/role.tf
@@ -5,7 +5,7 @@
 # - Used by ETL pipelines and data processing workflows
 # - Can be granted specific privileges on schemas/tables
 
-resource "snowflake_role" "transformer_role" {
+resource "snowflake_account_role" "transformer_role" {
   name    = var.transformer_role_name
   comment = "Data transformer role managed by Terraform (${var.env})"
 }
