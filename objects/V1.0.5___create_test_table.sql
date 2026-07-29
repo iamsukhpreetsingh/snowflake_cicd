fbcfc25b58658bad1efab381f8c83d23011c12de
Sukh <sukhpreetsingh@gmail.com>
2026-07-29 16:08:33 +0530

    corrections

--- objects/V1.0.5___create_test_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS IDENTIFIER('{{ db }}.EXTERNAL_STAGING.CUSTOMERS_1') (
-    customer_id     NUMBER(38,0)   NOT NULL,
-    customer_name   VARCHAR(200)   NOT NULL,
-    customer_category  VARCHAR(200),
-    phone_no           VARCHAR(255),
-    signup_date     DATE           NOT NULL,
-    is_active       BOOLEAN        DEFAULT TRUE,
-    created_at      TIMESTAMP_NTZ  DEFAULT CURRENT_TIMESTAMP()
-);
