29385624b210617ab90209d31bb94a7a27bc8956
Sukh <sukhpreetsingh@gmail.com>
2026-07-29 17:08:51 +0530

    dd

--- terraform/file_format.tf
+++ terraform/file_format.tf
@@ -10,7 +10,7 @@
 # - null_if: Values to treat as NULL
 # - compression: Auto-detect compression (GZIP, etc.)
 
-resource "snowflake_file_format" "csv_format_test" {
+resource "snowflake_file_format" "csv_format" {
   name     = var.file_format_name
   database = snowflake_database.app_database.name
   schema   = snowflake_schema.external_staging.name
