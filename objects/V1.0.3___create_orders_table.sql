f871eaf97de35522a31fae6215b141e4f68406bb
Sukh <sukhpreetsingh@gmail.com>
2026-07-29 16:01:40 +0530

    test

--- objects/V1.0.3___create_orders_table.sql
+++ objects/V1.0.3___create_orders_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS IDENTIFIER('{{ db }}.DEMO.ORDERS') (
+CREATE TABLE IF NOT EXISTS IDENTIFIER('{{ db }}.EXTERNAL_STAGING.ORDERS') (
     order_id           NUMBER IDENTITY(1,1),
     customer_id        NUMBER NOT NULL,
     product_id         NUMBER NOT NULL,
